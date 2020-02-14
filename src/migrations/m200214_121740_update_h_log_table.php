11bdf0ede62806182bc1ceaa65f8f141a851c812
forecho <caizhenghai@gmail.com>
2020-02-14 22:08:21 +0800

    fix: migrate file

--- src/migrations/m200214_121740_update_h_log_table.php
+++ src/migrations/m200214_121740_update_h_log_table.php
@@ -15,7 +15,7 @@ class m200214_121740_update_h_log_table extends Migration
         $this->addColumn("{{%h_log}}", 'fk',
             $this->string(100)->comment('外键')->defaultValue('')->after('h_log_template_id')
         );
-        $this->alterColumn("{{%h_log}}", 'fk',
+        $this->alterColumn("{{%h_log}}", 'username',
             $this->string(100)->defaultValue(null)->comment('用户名')
         );
         $this->createIndex('fk_h_log_template_id_fk', '{{%h_log}}', ['h_log_template_id', 'fk']);
@@ -27,10 +27,10 @@ class m200214_121740_update_h_log_table extends Migration
     public function safeDown()
     {
         echo "m200214_121740_update_h_log_table cannot be reverted.\n";
-        $this->alterColumn("{{%h_log}}", 'fk',
+        $this->alterColumn("{{%h_log}}", 'username',
             $this->string()->defaultValue(null)->comment('用户名')
         );
-        $this->dropColumn('fk_h_log_template_id_fk', '{{%h_log}}');
+        $this->dropIndex('fk_h_log_template_id_fk', '{{%h_log}}');
         $this->dropColumn("{{%h_log}}", 'fk');
         return true;
     }
